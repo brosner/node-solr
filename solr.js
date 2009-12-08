2f00a83a85f0a638ae2781dc97aac75fac71c1cc
Brian Rosner <brosner@gmail.com>
2009-12-07 19:12:19 -0700

    added Solr.add for adding documents to index and modified Solr.request to work with POST methods

--- solr.js
+++ solr.js
@@ -43,19 +43,33 @@ var Solr = function(url) {
     this.debug = true;
 }
 Solr.prototype = {
-    request: function(method, path, params, options) {
+    request: function(method, path, options) {
+        var debug = this.debug; // this is overridden in some callbacks
         var client = cacheClient(this.port, this.host);
-        params["wt"] = "json"
-        var path = this.path + path + "?" + buildQS(params);
-        if (this.debug) {
+        var path = this.path + path;
+        if (options.params) {
+            path += "?" + buildQS(options.params);
+        }
+        if (debug) {
             sys.puts("Solr.request START: " + method + " " + toJSON(path));
+        }
+        if (options.headers) {
+            headers = options.headers;
+        } else {
+            headers = {};
         }
         // @@@ this feels like a bug. i'd think node would at least do this
         // much for me.
-        headers = [
-            ["Host", this.host + ":" + this.port.toString()],
-        ]
-        var request = client.request("GET", path, headers);
+        headers["Host"] = this.host + ":" + this.port.toString();
+        var request;
+        if (options.body) {
+            var body = options.body.toString();
+            headers["Content-Length"] = body.length;
+            request = client.request(method, path, headers);
+            request.sendBody(body, "utf8");
+        } else {
+            request = client.request("GET", path, headers);
+        }
         request.finish(function(response) {
             var data = "";
             response.setBodyEncoding("utf8");
@@ -63,25 +77,53 @@ Solr.prototype = {
                 data += chunk;
             });
             response.addListener("complete", function() {
-                if (this.debug) {
-                    sys.puts("Solr.request COMPLETE: " + data);
+                if (debug) {
+                    sys.puts("Solr.request COMPLETE: "+response.statusCode+" "+toJSON(response.headers)+" "+data);
                 }
-                data = JSON.parse(data);
-                if (options.success) {
-                    options.success(data);
+                if (response.statusCode == 200) {
+                    data = JSON.parse(data);
+                    if (options.success) {
+                        options.success(data);
+                    }
                 }
             });
         });
     },
     search: function(q, callback) {
         params = {
-            q: q
+            q: q,
+            wt: "json"
         }
-        this.request("GET", "/select/", params, {
+        this.request("GET", "/select/", {
+            params: params,
             success: function(data) {
                 callback(data["response"]);
             }
         });
+    },
+    add: function(docs) {
+        if (this.debug) {
+            sys.puts("Solr.add: " + toJSON(docs));
+        }
+        var xml = [];
+        xml.push("<add>");
+        for (var i = 0; i < docs.length; i++) {
+            var doc = docs[i];
+            xml.push("<doc>");
+            for (k in doc) {
+                xml.push('<field name="'+k+'">'+doc[k].toString()+'</field>');
+            }
+            xml.push("</doc>");
+        }
+        xml.push("</add>");
+        if (this.debug) {
+            sys.puts("Solr.add XML: " + xml.join(""));
+        }
+        this.request("POST", "/update/", {
+            params: {wt: "json"},
+            headers: {"Content-Type": "text/xml"},
+            body: xml
+        })
     }
 }
 
